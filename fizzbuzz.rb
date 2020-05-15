7fad5ad5756be9d4a748e20caae18695f9c57bb8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-15 21:33:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0
-    "Buzz"
-  else
-    nil
+def fizzbuzz(max)
+  arr = []
+  (1..max).each do |num|
+    if num % 3 == 0 && num % 5 != 0
+      arr << num
+    end
+    if num % 3 != 0 && num % 5 == 0
+      arr << num
+    end
   end
+
+  return arr
 end
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(0)
-fizzbuzz(15)
+
+print fizzbuzz(20)
+ puts
